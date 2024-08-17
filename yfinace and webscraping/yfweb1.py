941ef68939a7249557dcf2e72ab33ad9b6d3cf27
knightallwell <162976641+knightallwell@users.noreply.github.com>
2024-08-17 22:25:51 +0100

    apply new

--- yfinace and webscraping/yfweb1.py
+++ yfinace and webscraping/yfweb1.py
@@ -7,42 +7,39 @@ from bs4 import BeautifulSoup
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import requests
-import datetime
-
 
 
 tsla= yf.Tickers('TSLA')
 tsla_data=tsla.tickers['TSLA'].history(period='max')
+tsla_data.reset_index(inplace=True)
 tsla_data.head()
 tsla_data.info()
 tsla_data.tail()
 gme= yf.Ticker('GME')
 gme_data=gme.history(period='max')
+gme_data.reset_index(inplace=True)
 gme_data.head()
 
 webtsla='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm'
-
-data_tsla=requests.get(webtsla)
-soup=BeautifulSoup(data_tsla.text, 'html')
-spup=soup.find('table')
-tslarevenew1=pd.read_html(str(spup))[0]
-tslarevenew1
-
+data_tsla=requests.get(webtsla).text
+soup=BeautifulSoup(data_tsla, 'html5lib')
+tsla_revenue=pd.read_html(data_tsla, match="Tesla Quarterly Revenue")[0]
+tsla_revenue.rename(inplace=True,columns={"Tesla Quarterly Revenue (Millions of US $)": "Date", "Tesla Quarterly Revenue (Millions of US $).1": "Revenue"})
+tsla_revenue
+tsla_revenue['Revenue']=tsla_revenue['Revenue'].str.replace(',|\$',"", regex=True)
+tsla_revenue.dropna(inplace=True)
+tsla_revenue=tsla_revenue[tsla_revenue['Revenue']!=""]
+tsla_revenue
 gmeweb='https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html'
-gme22=requests.get(gmeweb)
-gmedata=BeautifulSoup(gme22.text, 'html')
-gme_soup=gmedata.find('table')
-gme_soup
-gme_soup1=pd.read_html(str(gme_soup))[0]
-gme_soup1
-
-tsla_data
-tslarevenew1.tail()
-gme_data.head()
-gme_soup1.tail()
-
-tslarevenew1['Tesla Annual Revenue (Millions of US $).1']=tslarevenew1['Tesla Annual Revenue (Millions of US $).1'].str.replace(',|\$',"", regex=True)
-gme_soup1['GameStop Annual Revenue (Millions of US $).1']=gme_soup1['GameStop Annual Revenue (Millions of US $).1'].str.replace(',|\$',"", regex=True)
+gme22=requests.get(gmeweb).text
+gmedata=BeautifulSoup(gme22, 'html5lib')
+gme_revenue=pd.read_html(gme22,match="GameStop Quarterly Revenue")[0]
+gme_revenue.rename(inplace=True, columns={"GameStop Quarterly Revenue (Millions of US $)": "Date", "GameStop Quarterly Revenue (Millions of US $).1": "Revenue"})
+gme_revenue
+gme_revenue['Revenue']=gme_revenue['Revenue'].str.replace(',|\$',"", regex=True)
+gme_revenue.dropna(inplace=True)
+gme_revenue=gme_revenue[gme_revenue['Revenue']!=""]
+gme_revenue
 
 def make_graph(stock_data, revenue_data, stock):
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
@@ -60,37 +57,5 @@ def make_graph(stock_data, revenue_data, stock):
     xaxis_rangeslider_visible=True)
     fig.show()
 
-make_graph(gme_data,gme_soup1,'gme')
-
-sns.relplot(data=tsla_data, x='Stock Splits', y='Date', kind='line')
-plt.ylabel('Date')
-plt.xlabel('Stock Splits')
-plt.title('Tesla stock')
-plt.grid()
-plt.ylim()
-plt.tight_layout()
-
-sns.relplot(data=tslarevenew1, y='Tesla Annual Revenue (Millions of US $).1', x='Tesla Annual Revenue (Millions of US $)', kind='line')
-plt.ylabel('Revenue')
-plt.xlabel('Date')
-plt.title('Tesla stock')
-plt.grid()
-plt.ylim()
-plt.tight_layout()
-
-sns.relplot(data=gme_data, x='Stock Splits', y='Date', kind='line')
-plt.ylabel('Date')
-plt.xlabel('Stock Splits')
-plt.title('GameStop')
-plt.grid()
-plt.ylim()
-plt.tight_layout()
-
-sns.relplot(data=gme_soup1, y='GameStop Annual Revenue (Millions of US $).1', x='GameStop Annual Revenue (Millions of US $)', kind='line')
-plt.ylabel('Revenue')
-plt.xlabel('Date')
-plt.title('GameStop')
-plt.grid()
-plt.ylim()
-plt.tight_layout()
-
+make_graph(tsla_data,tsla_revenue,'Tesla')
+make_graph(gme_data,gme_revenue,'Tesla')
